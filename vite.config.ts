6a1e8ab9e1d75bf6941ccb8da436180daeff63e4
Yan Mantovani <yan0507@live.com>
2025-08-27 13:52:05 -0300

    Test SI v3

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+console.log("VERCEL_ENV:", process.env.VERCEL_ENV);
+console.log("NODE_ENV:", process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
