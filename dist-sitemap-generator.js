29ad3e1bc57276f0fe9f55206f91d5f62c41fb4e
Yan Mantovani <yan0507@live.com>
2025-11-06 10:24:34 -0300

    Update dist-sitemap-generator.js

--- dist-sitemap-generator.js
+++ dist-sitemap-generator.js
@@ -41,7 +41,7 @@ const blogPosts = [
   }
 ];
 
-// SEMPRE usar a versão sem www como canônica (oficial)
+// SEMPRE usar a versão sem www como canônica (oficial)a
 const baseUrl = 'https://yanmantovani.com';
 const currentDate = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD para sitemap
 
